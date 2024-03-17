a777e61577c1b732c556e8792943f10881f0fe78
Alexander Kizimenko <dctrback@bk.ru>
2024-03-17 18:31:10 +0300

    upds for credential storage

--- src/shared/internal-tools-interfaces/credential-interfaces.ts
+++ src/shared/internal-tools-interfaces/credential-interfaces.ts
@@ -1,17 +1,11 @@
-export interface CredentialOptions {
-  path?: string;
-  domain?: string;
-  expires?: Date | string;
-  maxAge?: number;
-  secure?: boolean;
-  samesite?: 'strict' | 'lax';
-
-  httpOnly?: boolean;
-}
+import {
+  RequestCookie,
+  ResponseCookie,
+} from 'next/dist/compiled/@edge-runtime/cookies';
 
 export interface CredentialStorageInterface {
-  setCredential(key: string, value: string, options: CredentialOptions): void;
-  getCredential(key: string): string | null | undefined;
+  setCredential(key: string, value: string, options: ResponseCookie): void;
+  getCredential(key: string): RequestCookie | undefined;
   removeCredential(key: string): void;
   invalidate(): void;
 }
--- src/shared/internal-tools/CredentialStorage.ts
+++ src/shared/internal-tools/CredentialStorage.ts
@@ -1,68 +1,31 @@
 'use client';
 
 import {
-  CredentialOptions,
-  CredentialStorageInterface,
-} from '../internal-tools-interfaces';
+  RequestCookie,
+  ResponseCookie,
+} from 'next/dist/compiled/@edge-runtime/cookies';
+import { cookies } from 'next/headers';
+import { CredentialStorageInterface } from '../internal-tools-interfaces';
 
 class CredentialStorage implements CredentialStorageInterface {
-  setCredential(key: string, value: string, options?: CredentialOptions): void {
-    if (options && options.expires instanceof Date) {
-      options.expires = options.expires.toUTCString();
-    }
+  protected CookieService = cookies();
 
-    let updatedCookie =
-      encodeURIComponent(key) + '=' + encodeURIComponent(value);
-
-    for (let optionKey in options) {
-      if (typeof optionKey !== 'string') {
-        continue;
-      }
-
-      let optionValue = options[optionKey];
-
-      if (optionKey === 'maxAge') {
-        optionKey = 'max-age';
-      }
-
-      updatedCookie += '; ' + optionKey;
-
-      if (optionValue !== true) {
-        updatedCookie += '=' + optionValue;
-      }
-    }
-
-    console.log(updatedCookie);
-
-    document.cookie = updatedCookie;
+  setCredential(key: string, value: string, options: ResponseCookie): void {
+    this.CookieService.set(key, value, options);
   }
 
-  getCredential(key: string): string | null | undefined {
-    let matches = document.cookie.match(
-      new RegExp(
-        '(?:^|; )' +
-          key.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') +
-          '=([^;]*)'
-      )
-    );
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+  getCredential(key: string): RequestCookie | undefined {
+    return this.CookieService.get(key);
   }
 
   removeCredential(key: string): void {
-    this.setCredential(key, '', {
-      maxAge: -1,
-    });
+    this.CookieService.delete(key);
   }
 
   invalidate(): void {
-    document.cookie
-      .split(';')
-      .forEach(
-        (c) =>
-          (document.cookie = c
-            .replace(/^ +/, '')
-            .replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/`))
-      );
+    for (let cookie in this.CookieService.getAll()) {
+      this.CookieService.delete(cookie);
+    }
   }
 }
 
